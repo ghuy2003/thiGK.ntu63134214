68d1ab2c1b25163b0e9f37fdee20a513ffb1daba
ghuy2003 <2003nguyengiahuy@gmail.com>
2024-03-26 15:12:30 +0700

    push1

---
+++ ghuy/src/main/java/com/example/ghuy/GhuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhuyApplication.class, args);
+	}
+
+}
---
+++ ghuy/src/test/java/com/example/ghuy/GhuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
