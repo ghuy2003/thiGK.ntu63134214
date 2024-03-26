04852104a36a32a795153ab0799537feed77b163
ghuy2003 <2003nguyengiahuy@gmail.com>
2024-03-26 15:27:35 +0700

    push2

--- nguyengiahuy_63134214/src/main/java/com/example/nguyengiahuy_63134214/Nguyengiahuy63134214Application.java
+++ nguyengiahuy_63134214/src/main/java/com/example/nguyengiahuy_63134214/Nguyengiahuy63134214Application.java
@@ -1,13 +1,13 @@
-package com.example.ghuy;
+package com.example.nguyengiahuy_63134214;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GhuyApplication {
+public class Nguyengiahuy63134214Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GhuyApplication.class, args);
+		SpringApplication.run(Nguyengiahuy63134214Application.class, args);
 	}
 
 }
--- nguyengiahuy_63134214/src/test/java/com/example/nguyengiahuy_63134214/Nguyengiahuy63134214ApplicationTests.java
+++ nguyengiahuy_63134214/src/test/java/com/example/nguyengiahuy_63134214/Nguyengiahuy63134214ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ghuy;
+package com.example.nguyengiahuy_63134214;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GhuyApplicationTests {
+class Nguyengiahuy63134214ApplicationTests {
 
 	@Test
 	void contextLoads() {
